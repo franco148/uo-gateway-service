4e7ebf5bcb006589a477fae2f28f5fd5601b740f
franco-extreme <franck_ral@hotmail.com>
2018-05-02 22:04:58 -0400

    uo-gateway-service: Initial structure of the project.

---
+++ src/main/java/com/fral/uo/gateway/uogatewayservice/UoGatewayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fral.uo.gateway.uogatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoGatewayServiceApplication.class, args);
+	}
+}
