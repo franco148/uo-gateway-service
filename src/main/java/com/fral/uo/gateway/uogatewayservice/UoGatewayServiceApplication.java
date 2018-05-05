e00fbf8c1ef7a68fbdca546af5f5c63d11f76b94
franco-extreme <franck_ral@hotmail.com>
2018-05-05 01:19:59 -0400

    uo-gateway-service: Initial configuration of gateway service and configured with discovery service.

--- src/main/java/com/fral/uo/gateway/uogatewayservice/UoGatewayServiceApplication.java
+++ src/main/java/com/fral/uo/gateway/uogatewayservice/UoGatewayServiceApplication.java
@@ -2,7 +2,11 @@ package com.fral.uo.gateway.uogatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class UoGatewayServiceApplication {
 
