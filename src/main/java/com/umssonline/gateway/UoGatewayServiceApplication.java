6f9a280cf826ee16bd17bfe0a5b08405a0f73536
franco-extreme <franck_ral@hotmail.com>
2018-07-24 16:48:18 -0400

    GatewayService-ApiDocs: Configured default initialization of swagger-uo. Swagger is integrated to the system..

--- src/main/java/com/umssonline/gateway/UoGatewayServiceApplication.java
+++ src/main/java/com/umssonline/gateway/UoGatewayServiceApplication.java
@@ -4,13 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
+@EnableSwagger2
 public class UoGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UoGatewayServiceApplication.class, args);
 	}
+
+    @Bean
+    UiConfiguration uiConfig() {
+
+        return new UiConfiguration("validatorUrl", "list", "alpha", "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+    }
 }
