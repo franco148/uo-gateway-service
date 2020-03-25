db68ff9532b4966d702624ee7c01882cfcc18908
franco-extreme <franck_ral@hotmail.com>
2020-03-25 17:33:59 -0400

    GatewayService-SecurityJwt: Allow Access to Registration and Login Urls

--- src/main/java/com/umssonline/gateway/security/WebSecurity.java
+++ src/main/java/com/umssonline/gateway/security/WebSecurity.java
@@ -3,6 +3,7 @@ package com.umssonline.gateway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, environment.getProperty("uo.zuul.security.h2-console-path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("uo.zuul.security.url-path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("uo.zuul.security.registration-path")).permitAll()
+                .anyRequest().authenticated();
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
